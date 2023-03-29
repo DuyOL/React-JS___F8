8e0f605ae3225089d38d7dbe1856d8e1f55c5aee
DuyOL <duyskt228@gmail.com>
2023-03-29 10:53:51 +0700

    29-03

---
+++ Enhanced object literals/README.md
@@ -0,0 +1,7 @@
+ ### F8
+ - Định Nghĩa Key : Value cho Object
+ - Định nghĩa Method cho Object
+
+ ### Định Nghĩa Enhanced object literals
+ - Enhanced object literals là một tính năng được giới thiệu trong ECMAScript 6 (ES6) của ngôn ngữ lập trình JavaScript, cho phép ta khai báo các đối tượng với cú pháp ngắn gọn hơn và nhiều tính năng mới hơn so với cú pháp truyền thống.
+ - Với Enhanced object literals, ta có thể sử dụng các biểu thức trong khai báo đối tượng, định nghĩa phương thức cho đối tượng bằng cú pháp ngắn gọn hơn, và đặt tên thuộc tính cho đối tượng bằng biến.
---
+++ Enhanced object literals/index.js
@@ -0,0 +1,32 @@
+// VD1
+var Name = 'Name';
+var Age = 'Age';
+var Address = 'Address';
+const Introduce = {
+    [Name]: 'NguyenDucDuy',
+    [Age]: 21,
+    [Address]: 'HaiDuong',
+}
+console.log(Introduce);
+
+// VD2
+var tencv = 'TenCV';
+var luong = 'Luong';
+var phucap = 'PhuCap';
+
+const tenCongty = {
+    [tencv]: 'DevMaster',
+    [luong]: 10.000,
+    [phucap]: 1000,
+};
+console.log(tenCongty)
+
+// Bài Tập Enhanced object literals F8
+function arrToObj(arr) {
+    let ob = {} // khởi tạo đối tượng kết quả là một đối tượng rỗng
+    arr.forEach((value) => { // duyệt qua mỗi phần tử của mảng arr bằng forEach
+        ob[value[0]] = value[1]; // tạo một thuộc tính trong đối tượng với key là phần tử đầu tiên của mảng con và value là phần tử thứ hai của mảng con.
+    })
+    return ob; // trả về đối tượng kết quả đã được tạo
+}
+
